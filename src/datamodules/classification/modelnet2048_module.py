685180e757a1c79829b0abbba8496ca622e72d2d
Chris Lee <git@clee.ai>
2022-11-14 14:33:24 -0700

    Remove extra param

--- src/datamodules/classification/modelnet2048_module.py
+++ src/datamodules/classification/modelnet2048_module.py
@@ -26,7 +26,6 @@ class ModelNet2048DataModule(LightningDataModule):
     def __init__(
         self,
         data_dir: str = "data/",
-        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
         batch_size: int = 64,
         num_workers: int = 0,
         pin_memory: bool = False,
