34fa78a7a871206a3d1872fb1f06aa00cc942997
Chris Lee <git@clee.ai>
2022-11-14 14:27:48 -0700

    Run precommit

--- src/models/openpoints_module.py
+++ src/models/openpoints_module.py
@@ -8,23 +8,23 @@ from torchmetrics import MaxMetric, MeanMetric
 from torchmetrics.classification.accuracy import Accuracy
 
 from openpoints.models import build_model_from_cfg
-from src.utils import pylogger
 from openpoints.utils import EasyConfig
-
+from src.utils import pylogger
 
 log = pylogger.get_pylogger(__name__)
 
+
 class OpenPointsModule(LightningModule):
-    """
-    LightningModule Docs:
-        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """LightningModule Docs:
+
+    https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
     """
 
     def __init__(
         self,
         net: omegaconf.DictConfig,
         optimizer: torch.optim.Optimizer,
-        lr_scheduler: dict, # todo: make this into a dataclass
+        lr_scheduler: dict,  # todo: make this into a dataclass
     ):
         super().__init__()
 
@@ -140,7 +140,10 @@ class OpenPointsModule(LightningModule):
             https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
         """
         optimizer = self.hparams.optimizer(params=self.parameters())
-        if self.hparams.lr_scheduler is not None and self.hparams.lr_scheduler.scheduler is not None:
+        if (
+            self.hparams.lr_scheduler is not None
+            and self.hparams.lr_scheduler.scheduler is not None
+        ):
             # print(self.hparams.scheduler)
             scheduler = self.hparams.lr_scheduler.scheduler(optimizer=optimizer)
             scheduler_config = omegaconf.OmegaConf.to_container(self.hparams.lr_scheduler.config)
