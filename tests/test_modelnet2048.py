d4a55e3aa2cc79c313caee7201fd4fda67256704
Elie-Alban LESCOUT <elie-alban.lescout@ensg.eu>
2022-11-18 19:22:47 +0100

    fixed test on modelnet2048 to reflect new changes in config

--- tests/test_modelnet2048.py
+++ tests/test_modelnet2048.py
@@ -3,18 +3,23 @@ from pathlib import Path
 import pytest
 import torch
 
+from src.datamodules.common import DataModuleConfig
 from src.datamodules.classification.modelnet2048_module import ModelNet2048DataModule
 
-
+@pytest.mark.slow
 @pytest.mark.parametrize("batch_size", [32, 128])
 def test_modelnet2048(batch_size):
-    data_dir = "data/modelnet2048"
+    config = DataModuleConfig(
+        data_dir="data/modelnet2048",
+        batch_size=batch_size
+    )
+ 
 
-    dm = ModelNet2048DataModule(data_dir=data_dir, batch_size=batch_size)
+    dm = ModelNet2048DataModule(config=config)
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_test
-    assert Path(data_dir).exists()
+    assert Path(config.data_dir).exists()
 
     dm.setup()
     assert dm.data_train and dm.data_test
