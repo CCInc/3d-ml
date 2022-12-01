9eda05b7c7e058594484518033db377637d5a7d3
Leo Stanislas <leo.stanislas@gmail.com>
2022-12-01 17:07:24 -0400

    PR review on modelnet test

--- tests/test_modelnet2048.py
+++ tests/test_modelnet2048.py
@@ -13,7 +13,6 @@ def test_modelnet2048(batch_size):
         data_dir="data/modelnet2048",
         batch_size=batch_size
     )
- 
 
     dm = ModelNet2048DataModule(config=config)
     dm.prepare_data()
@@ -24,13 +23,16 @@ def test_modelnet2048(batch_size):
     dm.setup()
     assert dm.data_train and dm.data_test
     assert dm.train_dataloader() and dm.test_dataloader()
+    assert dm.num_classes == 40
+    assert dm.num_feats == 0
 
     num_datapoints = len(dm.data_train) + len(dm.data_test)
     assert num_datapoints == 12_308
 
     batch = next(iter(dm.train_dataloader()))
+    assert batch.x is None
     x, y = batch.pos, batch.y
-    assert len(x) == batch_size
-    assert len(y) == batch_size
     assert x.dtype == torch.float32
+    assert x.size() == torch.Size([batch_size, 2048, 3])
     assert y.dtype == torch.int64
+    assert y.size() == torch.Size([batch_size])
