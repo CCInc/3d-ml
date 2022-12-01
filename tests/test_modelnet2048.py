8afb6a5d1a819f6ca3f25ec99e934b9804ed9f36
Leo Stanislas <leo.stanislas@gmail.com>
2022-12-01 17:48:09 -0400

    Cleanup

--- tests/test_modelnet2048.py
+++ tests/test_modelnet2048.py
@@ -1,16 +1,18 @@
-from pathlib import Path
+import os
+import shutil
 
 import pytest
 import torch
 
-from src.datamodules.common import DataModuleConfig
 from src.datamodules.classification.modelnet2048_module import ModelNet2048DataModule
+from src.datamodules.common import DataModuleConfig
+
 
 @pytest.mark.slow
 @pytest.mark.parametrize("batch_size", [32, 128])
-def test_modelnet2048(batch_size):
+def test_modelnet2048(tmp_path, batch_size):
     config = DataModuleConfig(
-        data_dir="data/modelnet2048",
+        data_dir=os.path.join(tmp_path, "modelnet2048"),
         batch_size=batch_size
     )
 
@@ -18,21 +20,22 @@ def test_modelnet2048(batch_size):
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_test
-    assert Path(config.data_dir).exists()
+    assert os.path.exists(config.data_dir)
 
     dm.setup()
     assert dm.data_train and dm.data_test
     assert dm.train_dataloader() and dm.test_dataloader()
     assert dm.num_classes == 40
     assert dm.num_feats == 0
-
-    num_datapoints = len(dm.data_train) + len(dm.data_test)
-    assert num_datapoints == 12_308
+    assert len(dm.data_train) == 9840
+    assert len(dm.data_test) == 2468
 
     batch = next(iter(dm.train_dataloader()))
     assert batch.x is None
-    x, y = batch.pos, batch.y
-    assert x.dtype == torch.float32
-    assert x.size() == torch.Size([batch_size, 2048, 3])
-    assert y.dtype == torch.int64
-    assert y.size() == torch.Size([batch_size])
+    assert batch.pos.dtype == torch.float32
+    assert batch.pos.size() == torch.Size([batch_size, 2048, 3])
+    assert batch.y.dtype == torch.int64
+    assert batch.y.size() == torch.Size([batch_size])
+
+    # rm data folder to save space
+    shutil.rmtree(config.data_dir)
--- tests/test_s3dis_1x1.py
+++ tests/test_s3dis_1x1.py
@@ -1,34 +1,41 @@
-import pytest
+import os
 import shutil
-from pathlib import Path
+
+import pytest
+import torch
 
 from src.datamodules.common import DataModuleConfig
 from src.datamodules.segmentation.s3dis_1x1_module import S3DIS1x1DataModule
+
 
 @pytest.mark.slow
 @pytest.mark.parametrize("batch_size", [32, 128])
 def test_s3dis_1x1(tmp_path, batch_size):
     config = DataModuleConfig(
-        data_dir=Path('data') / 's3dis_1x1',
+        data_dir=os.path.join(tmp_path, "s3dis_1x1"),
         batch_size=batch_size
     )
 
     dm = S3DIS1x1DataModule(config=config)
     dm.prepare_data()
     assert not dm.data_train and not dm.data_test
-
-    expected_data_location = Path(config.data_dir)
-    assert expected_data_location.exists()
+    assert os.path.exists(config.data_dir)
 
     dm.setup()
     assert dm.data_train and dm.data_test
     assert dm.train_dataloader() and dm.test_dataloader()
+    assert dm.num_classes == 13
+    assert dm.num_feats == 6
+    assert len(dm.data_train) == 20291
+    assert len(dm.data_test) == 3294
 
     batch = next(iter(dm.train_dataloader()))
-    x, y = batch.pos, batch.y
-    print(x.shape, y.shape)
-    assert len(x) == batch_size
-    assert len(y) == batch_size
+    assert batch.x.dtype == torch.float32
+    assert batch.x.size() == torch.Size([batch_size, 4096, 6])
+    assert batch.pos.dtype == torch.float32
+    assert batch.pos.size() == torch.Size([batch_size, 4096, 3])
+    assert batch.y.dtype == torch.int64
+    assert batch.y.size() == torch.Size([batch_size, 4096])
 
-    # rm created folder
-    shutil.rmtree(expected_data_location)
+    # rm data folder to save space
+    shutil.rmtree(tmp_path)
