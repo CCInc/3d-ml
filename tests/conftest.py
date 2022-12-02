8666241f6dd7327392c1db2f8ea1f1af5327b80e
Leo Stanislas <leo.stanislas@gmail.com>
2022-12-02 14:00:51 -0400

    Add segmentation tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pyrootutils
 import pytest
 from hydra import compose, initialize
@@ -12,6 +14,33 @@ def cfg_train_global() -> DictConfig:
             config_name="train.yaml",
             return_hydra_config=True,
             overrides=["model=cls_pointnet++", "data=cls_modelnet2048"],
+        )
+
+        # set defaults for all tests
+        with open_dict(cfg):
+            cfg.paths.root_dir = str(pyrootutils.find_root())
+            cfg.trainer.max_epochs = 1
+            cfg.trainer.limit_train_batches = 0.01
+            cfg.trainer.limit_val_batches = 0.1
+            cfg.trainer.limit_test_batches = 0.1
+            cfg.trainer.accelerator = "gpu"
+            cfg.trainer.devices = 1
+            cfg.data.datamodule.config.num_workers = 0
+            cfg.data.datamodule.config.pin_memory = False
+            cfg.extras.print_config = False
+            cfg.extras.enforce_tags = False
+            cfg.logger = None
+
+    return cfg
+
+
+@pytest.fixture(scope="package")
+def cfg_train_seg_global() -> DictConfig:
+    with initialize(version_base="1.2", config_path="../configs"):
+        cfg = compose(
+            config_name="train.yaml",
+            return_hydra_config=True,
+            overrides=["experiment=seg_s3dis1x1_pointnet++"],
         )
 
         # set defaults for all tests
@@ -62,6 +91,19 @@ def cfg_eval_global() -> DictConfig:
 @pytest.fixture(scope="function")
 def cfg_train(cfg_train_global, tmp_path) -> DictConfig:
     cfg = cfg_train_global.copy()
+
+    with open_dict(cfg):
+        cfg.paths.output_dir = str(tmp_path)
+        cfg.paths.log_dir = str(tmp_path)
+
+    yield cfg
+
+    GlobalHydra.instance().clear()
+
+
+@pytest.fixture(scope="function")
+def cfg_train_seg(cfg_train_seg_global, tmp_path) -> DictConfig:
+    cfg = cfg_train_seg_global.copy()
 
     with open_dict(cfg):
         cfg.paths.output_dir = str(tmp_path)
