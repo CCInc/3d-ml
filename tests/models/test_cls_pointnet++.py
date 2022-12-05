7d51f9d42aea277246d42397916872507618640e
Leo Stanislas <leo.stanislas@gmail.com>
2022-12-05 19:27:03 -0400

    Fix

--- tests/models/test_cls_pointnet++.py
+++ tests/models/test_cls_pointnet++.py
@@ -11,7 +11,7 @@ test_cases = [("cls_pointnet++.yaml", 2, 6, 1_462_978), ("cls_pointnet++.yaml", 
 @pytest.mark.parametrize(
     "cfg_model, num_classes, num_feats, num_params", test_cases, indirect=["cfg_model"]
 )
-def test_seg_pointnetpp(cfg_model, num_classes, num_feats, num_params):
+def test_cls_pointnetpp(cfg_model, num_classes, num_feats, num_params):
     model = hydra.utils.instantiate(cfg_model.model, num_classes=num_classes, num_feats=num_feats)
 
     assert count_trainable_params(model) == num_params
